1b1735c5e290ed0fc29ff394ef619447c17d5630
Alan Yao <alaney2@illinois.edu>
2023-11-01 11:50:45 -0500

    fix svg

--- src/components/PasswordChecks.tsx
+++ src/components/PasswordChecks.tsx
@@ -1,7 +1,7 @@
 export function UnfilledCheck() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" className="w-4 h-4" data-darkreader-inline-stroke="">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0z">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" className="w-4 h-4" data-darkreader-inline-stroke="">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0z">
       </path>
     </svg>
   )
@@ -18,7 +18,7 @@ export function FilledCheck() {
 
 export function OpenEye() {
   return (
-    <svg  xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" className="sbui-icon" data-darkreader-inline-stroke="">
+    <svg  xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" className="sbui-icon" data-darkreader-inline-stroke="">
       <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
       <circle cx="12" cy="12" r="3"></circle>
     </svg>
@@ -27,7 +27,7 @@ export function OpenEye() {
 
 export function ClosedEye() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" className="sbui-icon" data-darkreader-inline-stroke="">
+    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" className="sbui-icon" data-darkreader-inline-stroke="">
       <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24">
       </path>
       <line x1="1" y1="1" x2="23" y2="23"></line>
