d8951d993e1156d325a9969b26049ea5b860a875
Alan Yao <alaney2@illinois.edu>
2023-12-16 15:47:16 -0600

    mobile map

--- src/components/Dashboard1.tsx
+++ src/components/Dashboard1.tsx
@@ -8,15 +8,15 @@ export default function Dashboard1() {
 
   return (
     <>
-      <div className='lg:min-h-full flex flex-col bg-gray-100'>
+      <div className='min-h-full flex flex-col bg-gray-100'>
         <MobileSidebar />
         {/* Static sidebar for desktop */}
-        <div className="mx-auto flex w-full h-full items-start gap-x-8 lg:p-8 sm:px-6 lg:mt-8">
+        <div className="mx-auto flex w-full h-full items-start gap-x-8 py-8 lg:p-8 px-4 sm:px-6 lg:mt-8">
           <aside className={`lg:sticky top-16 left-4 flex-col mt-2 gap-y-5 overflow-y-auto absolute hidden lg:block`}>
             <DesktopSidebar />
           </aside>
 
-          <main className="flex-1 bg-white h-full rounded-3xl shadow-md hidden lg:block lg:mr-8">
+          <main className="flex-1 bg-white h-full rounded-3xl shadow-md lg:block lg:mr-8">
             {/* Main area */}
 
             {/* <DynamicMap /> */}
