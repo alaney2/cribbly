d063958a11180933be7facd226a19395513e3e22
Alan Yao <alanyao.training@gmail.com>
2024-01-12 15:01:03 -0800

    welcome

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,8 +1,73 @@
+"use client"
+import { WelcomeMap } from "@/components/WelcomeMap"
+import { SetStateAction, useState } from "react";
 
 export default function Welcome() {
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const steps = [
+    { name: 'Step 0', href: '#', status: 'complete' },
+    { name: 'Step 1', href: '#', status: 'complete' },
+    { name: 'Step 2', href: '#', status: 'current' },
+    { name: 'Step 3', href: '#', status: 'upcoming' },
+    { name: 'Step 4', href: '#', status: 'upcoming' },
+  ]
+
+  const currentStepIndex = steps.findIndex(step => step.status === 'current');
+
+  const handleStepClick = (stepIndex: number) => {
+    if (stepIndex <= currentStep) {
+      setCurrentStep(stepIndex);
+    }
+  };
+
+  const renderStepContent = (stepIndex: number) => {
+    switch(stepIndex) {
+      case 0:
+        return <div>Content for Step 0</div>;
+      case 1:
+        return <div>Content for Step 1</div>;
+      // Add cases for other steps
+      default:
+        return <div>Welcome! Please select a step.</div>;
+    }
+  };
+  
   return (
     <>
-      Welcome
+      <div className="w-full h-full px-32 pb-28 pt-16">
+        <WelcomeMap />
+        <nav className="flex items-center justify-center mt-8" aria-label="Progress">
+      {/* <p className="text-sm font-medium">
+        Step {steps.findIndex((step) => step.status === 'current') + 1} of {steps.length}
+      </p> */}
+          <ol role="list" className="flex items-center space-x-5">
+            {steps.map((step, index) => (
+              <li key={step.name} onClick={() => handleStepClick(index)}>
+                {step.status === 'complete' ? (
+                  <a href={step.href} className="block h-2.5 w-2.5 rounded-full bg-indigo-600 hover:bg-indigo-900">
+                    <span className="sr-only">{step.name}</span>
+                  </a>
+                ) : step.status === 'current' ? (
+                  <a href={step.href} className="relative flex items-center justify-center" aria-current="step">
+                    <span className="absolute flex h-5 w-5 p-px" aria-hidden="true">
+                      <span className="h-full w-full rounded-full bg-indigo-200" />
+                    </span>
+                    <span className="relative block h-2.5 w-2.5 rounded-full bg-indigo-600" aria-hidden="true" />
+                    <span className="sr-only">{step.name}</span>
+                  </a>
+                ) : (
+                  <a href={step.href} className="block h-2.5 w-2.5 rounded-full bg-gray-200 hover:bg-gray-400">
+                    <span className="sr-only">{step.name}</span>
+                  </a>
+                )}
+              </li>
+            ))}
+          </ol>
+        </nav>
+        {renderStepContent(currentStep)}
+
+      </div>
     </>
   )
 }
---
+++ src/components/WelcomeMap.tsx
@@ -0,0 +1,53 @@
+"use client"
+import { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+
+export function WelcomeMap() {
+  const token = process.env.NEXT_PUBLIC_MAPBOX_KEY!
+
+  useEffect(() => {
+
+    mapboxgl.accessToken = token
+
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/alan3y2/clq361ynz002t01ql64d81csd',
+      center: [-96, 37.8],
+      zoom: 3,
+    });
+
+    map.addControl(new mapboxgl.NavigationControl());
+
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      marker: new mapboxgl.Marker({ color: 'orange' }),
+      mapboxgl: mapboxgl
+    });
+
+
+    const geocoderContainer = document.getElementById('geocoder');
+    geocoderContainer!.innerHTML = '';
+    geocoderContainer!.appendChild(geocoder.onAdd(map));
+
+    geocoder.on('result', function (e) {
+      console.log(e.result);
+    });
+    console.log(geocoder.getWorldview())
+
+
+    return () => {
+      geocoderContainer!.innerHTML = '';
+    };
+
+  }, [token]);
+
+  return (
+    <div className='flex relative h-full w-full rounded-lg'>
+      <div id="geocoder" className="absolute z-10 w-1/2 top-4 left-1/2 transform -translate-x-1/2 flex items-center justify-center"></div>
+      <div id="map" className='absolute w-full h-full rounded-2xl'></div>
+    </div>
+  );
+}
