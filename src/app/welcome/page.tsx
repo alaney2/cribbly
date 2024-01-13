0879ad69850b8d21817eb1f830214b47db5565a9
Alan Yao <alanyao.training@gmail.com>
2024-01-12 17:30:19 -0800

    dots

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -3,17 +3,17 @@ import { WelcomeMap } from "@/components/WelcomeMap"
 import { SetStateAction, useState } from "react";
 
 export default function Welcome() {
-  const [currentStep, setCurrentStep] = useState(0);
+  const [currentStep, setCurrentStep] = useState(2);
 
   const steps = [
-    { name: 'Step 0', href: '#', status: 'complete' },
-    { name: 'Step 1', href: '#', status: 'complete' },
-    { name: 'Step 2', href: '#', status: 'current' },
-    { name: 'Step 3', href: '#', status: 'upcoming' },
-    { name: 'Step 4', href: '#', status: 'upcoming' },
+    { name: 'Step 0', href: '' },
+    { name: 'Step 1', href: '' },
+    { name: 'Step 2', href: '' },
+    { name: 'Step 3', href: '' },
+    { name: 'Step 4', href: '' },
   ]
 
-  const currentStepIndex = steps.findIndex(step => step.status === 'current');
+  // const currentStepIndex = steps.findIndex(step => step.status === 'current');
 
   const handleStepClick = (stepIndex: number) => {
     if (stepIndex <= currentStep) {
@@ -27,7 +27,12 @@ export default function Welcome() {
         return <div>Content for Step 0</div>;
       case 1:
         return <div>Content for Step 1</div>;
-      // Add cases for other steps
+      case 2:
+        return <WelcomeMap />;
+      case 3:
+        return <div>Content for Step 3</div>;
+      case 4:
+        return <div>Content for Step 4</div>;
       default:
         return <div>Welcome! Please select a step.</div>;
     }
@@ -36,28 +41,18 @@ export default function Welcome() {
   return (
     <>
       <div className="w-full h-full px-32 pb-28 pt-16">
-        <WelcomeMap />
+        {renderStepContent(currentStep)}
+
         <nav className="flex items-center justify-center mt-8" aria-label="Progress">
-      {/* <p className="text-sm font-medium">
-        Step {steps.findIndex((step) => step.status === 'current') + 1} of {steps.length}
-      </p> */}
           <ol role="list" className="flex items-center space-x-5">
             {steps.map((step, index) => (
               <li key={step.name} onClick={() => handleStepClick(index)}>
-                {step.status === 'complete' ? (
-                  <a href={step.href} className="block h-2.5 w-2.5 rounded-full bg-indigo-600 hover:bg-indigo-900">
-                    <span className="sr-only">{step.name}</span>
-                  </a>
-                ) : step.status === 'current' ? (
-                  <a href={step.href} className="relative flex items-center justify-center" aria-current="step">
-                    <span className="absolute flex h-5 w-5 p-px" aria-hidden="true">
-                      <span className="h-full w-full rounded-full bg-indigo-200" />
-                    </span>
-                    <span className="relative block h-2.5 w-2.5 rounded-full bg-indigo-600" aria-hidden="true" />
+                {index === currentStep ? (
+                  <a className="block h-2.5 w-2.5 rounded-full bg-indigo-600 hover:bg-indigo-900">
                     <span className="sr-only">{step.name}</span>
                   </a>
                 ) : (
-                  <a href={step.href} className="block h-2.5 w-2.5 rounded-full bg-gray-200 hover:bg-gray-400">
+                  <a className="block h-2.5 w-2.5 rounded-full bg-gray-200 hover:bg-gray-400">
                     <span className="sr-only">{step.name}</span>
                   </a>
                 )}
@@ -65,7 +60,6 @@ export default function Welcome() {
             ))}
           </ol>
         </nav>
-        {renderStepContent(currentStep)}
 
       </div>
     </>
