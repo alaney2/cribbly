9d23093016877f41e109bb72e5753f9d5557116b
Alan Yao <alaney2@illinois.edu>
2024-02-08 01:44:55 -0600

    country

--- src/components/welcome/ConfirmAddress.tsx
+++ src/components/welcome/ConfirmAddress.tsx
@@ -9,22 +9,25 @@ export function ConfirmAddress() {
       const result = JSON.parse(localStorage.getItem("result") || '{}');
       const address = result.place_name;
       const addressArray = address.split(',');
-      const city = addressArray[1].trim();
-      const state = addressArray[2].trim();
+      const numSlices = addressArray.length;
+      const country = addressArray.pop().trim();
+      // const state = addressArray[2].trim();
+
+      // const city = addressArray[1].trim();
       // const country = addressArray[3].trim();
-      console.log('city', city);
-      console.log('state', state);
+      // console.log('city', city);
+      // console.log('state', state);
       // console.log('country', country);
       console.log('result', result);
       console.log('address', address);
       console.log('Country', Country.getAllCountries());
     }
-
   }, )
   
   return (
     <>
       <h1>Confirm Address</h1>
+
     </>
   )
 }
