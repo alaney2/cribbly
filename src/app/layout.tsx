b271a640e00c3ca3020f69d04071266fda6d3ba9
Alan Yao <alanyao.training@gmail.com>
2024-02-09 16:52:43 -0600

    meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,9 @@ export default function RootLayout({
         lexend.variable,
       )}
     >
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+      {/* <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"></meta> */}
       <meta property="twitter:image" content="../images/icon.png"></meta>
       <meta property="twitter:card" content="../images/icon.png"></meta>
       <meta property="twitter:title" content="Property Management Software for Small Landlords | Cribbly"></meta>
