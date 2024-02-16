d1617275d028c7bc86e73bb9176e483d4671ef30
Alan Yao <alaney2@illinois.edu>
2024-02-16 01:00:05 -0600

    not found page

--- src/components/default/SlimLayout.tsx
+++ src/components/default/SlimLayout.tsx
@@ -9,7 +9,7 @@ import 'react-loading-skeleton/dist/skeleton.css'
 
 const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
-export function SlimLayout({ children, splineLink }: { children: React.ReactNode; splineLink: string }) {
+export function SlimLayout({ children, splineLink }: { children: React.ReactNode; splineLink?: string }) {
   
   const [isSplineLoaded, setIsSplineLoaded] = useState(false)
   const [isLargeScreen, setIsLargeScreen] = useState(false)
@@ -33,8 +33,6 @@ export function SlimLayout({ children, splineLink }: { children: React.ReactNode
 
   return (
     <>
-      {/* <link rel="preload" href="https://prod.spline.design/zRVkT5sl31cwbtW2/scene.splinecode" as="fetch"></link> */}
-
       <div className="relative flex min-h-full justify-center md:px-12 lg:px-0 bg-gray-100">
         <div className="relative z-10 flex flex-1 flex-col  px-4 py-10 justify-center md:flex-none md:px-28">
           <main className="mx-auto w-full max-w-md sm:px-4 md:w-96 md:max-w-sm md:px-0">
@@ -43,14 +41,17 @@ export function SlimLayout({ children, splineLink }: { children: React.ReactNode
         </div>
         {isLargeScreen && (
           <div className="hidden lg:relative lg:block lg:flex-1 leading-4">
-            {!isSplineLoaded && <Skeleton containerClassName="flex-1" height="100%" className="bg-opacity-0.1 opacity-0.05 z-10"/>}
-              <Spline 
-                scene={splineLink}
-                className="absolute inset-0 h-full w-full object-cover z-10 animate__animated animate__fadeIn animate__faster"
-                onLoad={handleSplineLoaded}
-              />
-            {/* </Suspense> */}
-            {/* <Image src={backgroundDefault} alt="" className="absolute inset-0 h-full w-full object-cover z-0 opacity-0.05" /> */}
+            {!splineLink && <Image src={backgroundDefault} alt="" className="absolute inset-0 h-full w-full object-cover z-0" />}
+            {splineLink && (
+              <>
+                {!isSplineLoaded && <Skeleton containerClassName="flex-1" height="100%" className="bg-opacity-0.1 opacity-0.05 z-10"/>}
+                <Spline 
+                  scene={splineLink}
+                  className="absolute inset-0 h-full w-full object-cover z-10 animate__animated animate__fadeIn animate__faster"
+                  onLoad={handleSplineLoaded}
+                />
+              </>
+            )}
           </div>
         )}
       </div>
