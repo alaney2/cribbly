beaacc65972d5448f53fb35951a5b0ab76005c43
Alan Yao <alanyao.training@gmail.com>
2024-03-13 00:51:53 -0700

    text

--- src/components/welcome/Checkout.tsx
+++ src/components/welcome/Checkout.tsx
@@ -7,22 +7,23 @@ import { Button } from '@/components/default/Button'
 import { Switch, SwitchField } from '@/components/catalyst/switch'
 import { Field as HeadlessField } from '@headlessui/react'
 import { Label } from '@/components/catalyst/fieldset'
+import Link from 'next/link'
 
 const tiers = [
   {
     name: 'Subscription',
     id: 'tier-hobby',
-    href: '#',
-    priceMonthly: '$20',
-    priceYearly: '$15',
+    href: '',
+    priceMonthly: 20,
+    priceYearly: 15,
     description: 'Modi dolorem expedita deleniti. Corporis iste qui inventore pariatur adipisci vitae.',
     features: ['Tiered pricing', 'Ability to switch between monthly and yearly plans', '48-hour support response time'],
   },
   {
     name: 'Lifetime',
     id: 'tier-team',
-    href: '#',
-    priceMonthly: '$450',
+    href: '',
+    price: 450,
     description: 'Explicabo quo fugit vel facere ullam corrupti non dolores. Expedita eius sit sequi.',
     features: [
       'Unlimited updates',
@@ -39,7 +40,7 @@ export function Checkout({ user, subscription, products }: { user: any, subscrip
     <div className="isolate overflow-hidden bg-gray-900">
       <div className="mx-auto max-w-7xl px-6 pb-96 pt-24 text-center sm:pt-32 lg:px-8">
         <div className="mx-auto max-w-4xl">
-          <h2 className="text-base font-semibold leading-7 text-indigo-400">Pricing</h2>
+          {/* <h2 className="text-base font-semibold leading-7 text-indigo-400">Pricing</h2> */}
           <p className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
             The right price for you, <br className="hidden sm:inline lg:hidden" />
             whoever you are
@@ -86,23 +87,34 @@ export function Checkout({ user, subscription, products }: { user: any, subscrip
                     </div>
                     {tier.name === 'Subscription' ? (
                       <>
-                        <div className="mt-4 flex items-baseline gap-x-2">
-                          <span className="text-5xl font-bold tracking-tight text-gray-900">
-                            <span className="ml-2">
-                              {yearly ? tier.priceYearly : tier.priceMonthly}
+                        <div className="mt-4 flex flex-col items-start gap-x-2">
+                          <div className="flex items-baseline">
+                            <span className="text-5xl font-bold tracking-tight text-gray-900">
+                              <span className="ml-2">
+                                ${yearly ? tier.priceYearly : tier.priceMonthly}
+                              </span>
                             </span>
-                          </span>
-                          <span className="text-base font-semibold leading-7 text-gray-600">
-                            /month
+                            <span className="text-base font-semibold leading-7 text-gray-600">
+                              /month
+                            </span>
+                          </div>
+                          <span className="text-sm text-gray-600 ml-2">
+                            ${yearly ? tier!.priceYearly! * 12 : tier.priceMonthly} billed {yearly ? "annually" : "monthly"}
                           </span>
                         </div>
                       </>
                     )
                     : (
                       <>
-                        <div className="mt-4 flex items-baseline gap-x-2">
-                          <span className="text-5xl font-bold track">
-                          {tier.priceMonthly}
+                        <div className="mt-4 flex flex-col items-start gap-x-2">
+                      
+                          <div className="flex items-baseline">
+                            <span className="text-5xl font-bold track">
+                            ${tier.price}
+                            </span>
+                          </div>
+                          <span className="text-sm text-gray-600 ml-2">
+                            One-time purchase
                           </span>
                         </div>
                       </>
@@ -117,13 +129,13 @@ export function Checkout({ user, subscription, products }: { user: any, subscrip
                       ))}
                     </ul>
                   </div>
-                  <a
+                  <Link
                     href={tier.href}
                     aria-describedby={tier.id}
                     className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
-                    Get started today
-                  </a>
+                    Select
+                  </Link>
                 </div>
               ))}
             </div>
