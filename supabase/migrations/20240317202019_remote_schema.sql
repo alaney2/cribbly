4c2b7c34f457539320ba5f3877a0eb85041d9c6d
Alan Yao <alanyao.training@gmail.com>
2024-03-17 13:26:46 -0700

    migration

---
+++ supabase/migrations/20240317202019_remote_schema.sql
@@ -0,0 +1,245 @@
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+CREATE EXTENSION IF NOT EXISTS "pg_net" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
+
+CREATE SCHEMA IF NOT EXISTS "public";
+
+ALTER SCHEMA "public" OWNER TO "pg_database_owner";
+
+COMMENT ON SCHEMA "public" IS 'standard public schema';
+
+CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
+
+CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgjwt" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
+
+CREATE TYPE "public"."pricing_plan_interval" AS ENUM (
+    'day',
+    'week',
+    'month',
+    'year'
+);
+
+ALTER TYPE "public"."pricing_plan_interval" OWNER TO "postgres";
+
+CREATE TYPE "public"."pricing_type" AS ENUM (
+    'one_time',
+    'recurring'
+);
+
+ALTER TYPE "public"."pricing_type" OWNER TO "postgres";
+
+CREATE TYPE "public"."subscription_status" AS ENUM (
+    'trialing',
+    'active',
+    'canceled',
+    'incomplete',
+    'incomplete_expired',
+    'past_due',
+    'unpaid',
+    'paused'
+);
+
+ALTER TYPE "public"."subscription_status" OWNER TO "postgres";
+
+CREATE OR REPLACE FUNCTION "public"."handle_new_user"() RETURNS "trigger"
+    LANGUAGE "plpgsql" SECURITY DEFINER
+    AS $$
+begin
+  insert into public.users (id, full_name, email)
+  values (
+    new.id, 
+    new.raw_user_meta_data->>'full_name', 
+    new.email
+  );
+  return new;
+end;
+$$;
+
+ALTER FUNCTION "public"."handle_new_user"() OWNER TO "postgres";
+
+SET default_tablespace = '';
+
+SET default_table_access_method = "heap";
+
+CREATE TABLE IF NOT EXISTS "public"."customers" (
+    "id" "uuid" NOT NULL,
+    "stripe_customer_id" "text" NOT NULL
+);
+
+ALTER TABLE "public"."customers" OWNER TO "postgres";
+
+CREATE TABLE IF NOT EXISTS "public"."prices" (
+    "id" "text" NOT NULL,
+    "product_id" "text" NOT NULL,
+    "active" boolean,
+    "unit_amount" bigint,
+    "currency" "text",
+    "type" "public"."pricing_type",
+    "interval" "public"."pricing_plan_interval",
+    "interval_count" integer,
+    "trial_period_days" integer,
+    "metadata" "jsonb",
+    "billing_scheme" "text",
+    "tax_behavior" "text",
+    "tiers_mode" "text"
+);
+
+ALTER TABLE "public"."prices" OWNER TO "postgres";
+
+CREATE TABLE IF NOT EXISTS "public"."products" (
+    "id" "text" NOT NULL,
+    "active" boolean,
+    "name" "text",
+    "description" "text",
+    "image" "text",
+    "metadata" "jsonb"
+);
+
+ALTER TABLE "public"."products" OWNER TO "postgres";
+
+CREATE TABLE IF NOT EXISTS "public"."subscriptions" (
+    "id" "text" NOT NULL,
+    "user_id" "uuid" DEFAULT "gen_random_uuid"(),
+    "status" "public"."subscription_status",
+    "metadata" "jsonb",
+    "price_id" "text",
+    "quantity" integer,
+    "cancel_at_period_end" boolean,
+    "created" timestamp with time zone,
+    "current_period_start" timestamp with time zone,
+    "current_period_end" timestamp with time zone,
+    "ended_at" timestamp with time zone,
+    "cancel_at" timestamp with time zone,
+    "canceled_at" timestamp with time zone,
+    "trial_start" timestamp with time zone,
+    "trial_end" timestamp with time zone
+);
+
+ALTER TABLE "public"."subscriptions" OWNER TO "postgres";
+
+CREATE TABLE IF NOT EXISTS "public"."users" (
+    "id" "uuid" NOT NULL,
+    "full_name" "text",
+    "welcome_screen" boolean DEFAULT true,
+    "created_at" timestamp with time zone DEFAULT "now"(),
+    "email" "text"
+);
+
+ALTER TABLE "public"."users" OWNER TO "postgres";
+
+ALTER TABLE ONLY "public"."customers"
+    ADD CONSTRAINT "customers_pkey" PRIMARY KEY ("id");
+
+ALTER TABLE ONLY "public"."prices"
+    ADD CONSTRAINT "prices_pkey" PRIMARY KEY ("id");
+
+ALTER TABLE ONLY "public"."products"
+    ADD CONSTRAINT "products_pkey" PRIMARY KEY ("id");
+
+ALTER TABLE ONLY "public"."users"
+    ADD CONSTRAINT "profiles_pkey" PRIMARY KEY ("id");
+
+ALTER TABLE ONLY "public"."subscriptions"
+    ADD CONSTRAINT "subscriptions_pkey" PRIMARY KEY ("id");
+
+ALTER TABLE ONLY "public"."users"
+    ADD CONSTRAINT "users_email_key" UNIQUE ("email");
+
+ALTER TABLE ONLY "public"."users"
+    ADD CONSTRAINT "profiles_id_fkey" FOREIGN KEY ("id") REFERENCES "auth"."users"("id") ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."customers"
+    ADD CONSTRAINT "public_customers_id_fkey" FOREIGN KEY ("id") REFERENCES "auth"."users"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."prices"
+    ADD CONSTRAINT "public_prices_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."subscriptions"
+    ADD CONSTRAINT "public_subscriptions_price_id_fkey" FOREIGN KEY ("price_id") REFERENCES "public"."prices"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."subscriptions"
+    ADD CONSTRAINT "public_subscriptions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+CREATE POLICY "Allow public read-only access." ON "public"."prices" FOR SELECT USING (true);
+
+CREATE POLICY "Allow public read-only access." ON "public"."products" FOR SELECT USING (true);
+
+CREATE POLICY "Can only view own subscriptions' data." ON "public"."subscriptions" FOR SELECT USING (("auth"."uid"() = "user_id"));
+
+CREATE POLICY "Enable all actions for users based on user_id" ON "public"."users" USING (("auth"."uid"() = "id")) WITH CHECK (("auth"."uid"() = "id"));
+
+ALTER TABLE "public"."customers" ENABLE ROW LEVEL SECURITY;
+
+ALTER TABLE "public"."prices" ENABLE ROW LEVEL SECURITY;
+
+ALTER TABLE "public"."products" ENABLE ROW LEVEL SECURITY;
+
+ALTER TABLE "public"."subscriptions" ENABLE ROW LEVEL SECURITY;
+
+ALTER TABLE "public"."users" ENABLE ROW LEVEL SECURITY;
+
+GRANT USAGE ON SCHEMA "public" TO "postgres";
+GRANT USAGE ON SCHEMA "public" TO "anon";
+GRANT USAGE ON SCHEMA "public" TO "authenticated";
+GRANT USAGE ON SCHEMA "public" TO "service_role";
+
+GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "anon";
+GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "authenticated";
+GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "service_role";
+
+GRANT ALL ON TABLE "public"."customers" TO "anon";
+GRANT ALL ON TABLE "public"."customers" TO "authenticated";
+GRANT ALL ON TABLE "public"."customers" TO "service_role";
+
+GRANT ALL ON TABLE "public"."prices" TO "anon";
+GRANT ALL ON TABLE "public"."prices" TO "authenticated";
+GRANT ALL ON TABLE "public"."prices" TO "service_role";
+
+GRANT ALL ON TABLE "public"."products" TO "anon";
+GRANT ALL ON TABLE "public"."products" TO "authenticated";
+GRANT ALL ON TABLE "public"."products" TO "service_role";
+
+GRANT ALL ON TABLE "public"."subscriptions" TO "anon";
+GRANT ALL ON TABLE "public"."subscriptions" TO "authenticated";
+GRANT ALL ON TABLE "public"."subscriptions" TO "service_role";
+
+GRANT ALL ON TABLE "public"."users" TO "anon";
+GRANT ALL ON TABLE "public"."users" TO "authenticated";
+GRANT ALL ON TABLE "public"."users" TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "service_role";
+
+RESET ALL;
