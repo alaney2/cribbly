1eb23d25b574e52d0092d568e006b274893446a8
Alan Yao <alaney2@illinois.edu>
2024-03-24 18:59:43 -0500

    lock

--- src/components/PropertiesGrid.tsx
+++ src/components/PropertiesGrid.tsx
@@ -42,13 +42,13 @@ export function PropertiesGrid() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-8">
       {properties.map((property) => (
-        <div key={property.id} className="bg-white shadow-md rounded-lg p-4">
+        <div key={property.id} className="rounded-lg p-4 h-36 shadow-sm bg-gray-50 ring-1 ring-gray-300">
           <h3 className="text-lg font-semibold">{property.street_address}</h3>
           <p>{property.apt}</p>
           <p>{property.city}, {property.state} {property.zip}</p>
-          <p>{property.country}</p>
+          {/* <p>{property.country}</p> */}
         </div>
       ))}
     </div>
