388531497c90d8f5d644dc0f2681be8aefd68207
Alan Yao <alaney2@illinois.edu>
2024-03-26 16:19:33 -0500

    layout

---
+++ src/app/dashboard/properties/layout.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import { MobileSidebar } from '@/components/MobileSidebar';
+import { DesktopSidebar } from '@/components/DesktopSidebar'
+// import { DesktopSidebarDashboard } from '@/components/DesktopSidebarDashboard'
+
+import { MapBox } from '@/components/MapBox';
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+  return (
+    <>
+      <div className='min-h-screen overflow-hidden flex flex-col bg-gray-100 overscroll-none'>
+        <MobileSidebar />
+        <div className="mx-auto flex w-full h-full items-start gap-x-8 pt-4 lg:px-8 px-4 sm:px-6 lg:mt-8">
+          <aside className={`lg:sticky top-16 left-4 flex-col mt-2 gap-y-5 overflow-y-auto absolute hidden lg:block`}>
+            <DesktopSidebar />
+            {/* <DesktopSidebarDashboard /> */}
+          </aside>
+
+          <main className="flex-1 bg-white h-full rounded-t-3xl shadow-md lg:block lg:mr-8 overflow-auto">
+            {children}
+          </main>
+        </div>
+      </div>
+    </>
+  )
+}
