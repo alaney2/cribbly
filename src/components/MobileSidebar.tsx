3519a25f41f1b3c6add124feecaf2ba0579e369d
Alan Yao <alanyao.training@gmail.com>
2024-03-30 17:42:10 -0500

    sidebar

--- src/components/MobileSidebar.tsx
+++ src/components/MobileSidebar.tsx
@@ -66,7 +66,7 @@ export function MobileSidebar({ user }: { user: any }) {
         <ProfileButton user={user} />
       </div>
 
-      <div className={`fixed inset-0 flex lg:hidden mt-16 z-40 ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
+      <div className={`fixed inset-0 flex lg:hidden mt-16 z-40 ${sidebarOpen ? 'fixed w-full h-full translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out bg-gray-100`}>
         <div className="flex grow flex-col overflow-y-auto bg-gray-100 px-4">
           <nav className="flex flex-1 flex-col">
             <ul role="list" className="flex flex-1 flex-col divide-y divide-gray-200">
