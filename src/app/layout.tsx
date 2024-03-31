3a815962c8a3cc13ec97b93499079f6da7e0a609
Alan Yao <alanyao.training@gmail.com>
2024-03-31 01:47:15 -0400

    bounce test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,7 +88,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
         `}
       </Script>
 
-      <body className="h-full bg-gray-50 overscroll-none lg:overscroll-auto">
+      <body className="h-full bg-gray-50 overscroll-none lg:overscroll-auto disable-bounce">
         <Toaster position="top-center" />
         {children}
         <Analytics />
