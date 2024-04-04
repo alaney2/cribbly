d9a8512b083fd57745236797087dcd09a8209a69
Alan Yao <alaney2@illinois.edu>
2024-04-04 00:53:01 -0500

    af

--- src/components/auth/SignInUp.tsx
+++ src/components/auth/SignInUp.tsx
@@ -105,7 +105,7 @@ export function SignInUp({ signIn, splineLink } : { signIn : boolean; splineLink
                     onChange={handleInputChange}
                     // required={true}
                     autoComplete='off'
-                    // autoFocus
+                    autoFocus
                   />
                 )}
                 {showInvalidEmail && (
