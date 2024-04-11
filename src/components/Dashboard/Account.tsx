3338a25241e174731e7bf9397b3c5205121a956d
Alan Yao <alaney2@illinois.edu>
2024-04-11 13:59:54 -0500

    loading

--- src/components/Dashboard/Account.tsx
+++ src/components/Dashboard/Account.tsx
@@ -185,9 +185,11 @@ export function Account() {
   }, [linkToken, ready, open]);
 
   return (
-    <div className="p-6 md:p-8 content-container">
+    <div className="p-6 md:p-8 content-container relative">
       {(isLoading || bankIsLoading) ? (
+        <div className="absolute inset-x-1/2 inset-y-1/2">
         <Spinner />
+        </div>
     ) : (
       <main className="px-4 py-4 sm:px-6 lg:flex-auto lg:px-4 lg:py-4">
         <div className="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
--- src/components/FuelSpinner.tsx
+++ src/components/FuelSpinner.tsx
@@ -1,7 +1,7 @@
 // Spinner.tsx
 export function Spinner() {
   return (
-    <div className="flex items-center justify-center animate-spin w-full">
+    <>
       <svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg" className="w-20 h-20">
         <circle className="spin2" cx="400" cy="400" fill="none" r="200" strokeWidth="50" stroke="#3b82f6" strokeDasharray="700 1400" strokeLinecap="round" />
       </svg>
@@ -31,6 +31,6 @@ export function Spinner() {
           animation-direction: alternate;
         }
       `}</style>
-    </div>
+    </>
   );
 }
