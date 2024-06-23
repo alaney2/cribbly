b5f3001af2733c69205a14e18eb6572e79d2e0a4
Alan Yao <alanyao.training@gmail.com>
2024-06-23 02:06:00 -0400

    overscroll

--- src/app/dashboard/[property_id]/delete-property/page.tsx
+++ src/app/dashboard/[property_id]/delete-property/page.tsx
@@ -6,7 +6,7 @@ export default function CurrentPropertySettings({ params } : { params: { propert
     <>
       <div className="justify-around flex h-full" >
         <main className="flex-auto lg:px-4 lg:py-4 w-full space-y-4 sm:space-y-8 flex justify-center items-center min-h-full">
-          <div className="grid gap-8">
+          <div className="grid mb-16">
             <div className="">
               <DeleteCard propertyId={params.property_id}/>
             </div>
