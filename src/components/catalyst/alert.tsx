946f3930157b0202c984d9cdf23ec63ac377ad94
Alan Yao <alaney2@illinois.edu>
2024-06-29 13:36:42 -0500

    alert

--- src/components/catalyst/alert.tsx
+++ src/components/catalyst/alert.tsx
@@ -16,6 +16,8 @@ const sizes = {
 }
 
 export function Alert({
+  open,
+  onClose,
   size = 'md',
   className,
   children,
@@ -25,28 +27,45 @@ export function Alert({
   'className'
 >) {
   return (
-    <Headless.Dialog {...props}>
-      <Headless.DialogBackdrop
-        transition
-        className="fixed inset-0 flex w-screen justify-center overflow-y-auto bg-zinc-950/15 px-2 py-2 transition duration-100 focus:outline-0 data-[closed]:opacity-0 data-[enter]:ease-out data-[leave]:ease-in sm:px-6 sm:py-8 lg:px-8 lg:py-16 dark:bg-zinc-950/50"
-      />
+    <Headless.Transition show={open} {...props}>
+      <Headless.Dialog onClose={onClose}>
+        <Headless.Transition.Child
+          enter="transition duration-100 ease-out"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition duration-100 ease-in"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Headless.DialogBackdrop
+            className="fixed inset-0 flex w-screen justify-center overflow-y-auto bg-zinc-950/15 px-2 py-2 focus:outline-0 sm:px-6 sm:py-8 lg:px-8 lg:py-16 dark:bg-zinc-950/50"
+          />
+        </Headless.Transition.Child>
 
-      <div className="fixed inset-0 w-screen overflow-y-auto pt-6 sm:pt-0">
-        <div className="grid min-h-full grid-rows-[1fr_auto_1fr] justify-items-center p-8 sm:grid-rows-[1fr_auto_3fr] sm:p-4">
-          <Headless.DialogPanel
-            transition
-            className={clsx(
-              className,
-              sizes[size],
-              'row-start-2 w-full rounded-2xl bg-white p-8 shadow-lg ring-1 ring-zinc-950/10 sm:rounded-2xl sm:p-6 dark:bg-zinc-900 dark:ring-white/10 forced-colors:outline',
-              'transition duration-100 data-[closed]:data-[enter]:scale-95 data-[closed]:opacity-0 data-[enter]:ease-out data-[leave]:ease-in'
-            )}
-          >
-            {children}
-          </Headless.DialogPanel>
+        <div className="fixed inset-0 w-screen overflow-y-auto pt-6 sm:pt-0">
+          <div className="grid min-h-full grid-rows-[1fr_auto_1fr] justify-items-center p-8 sm:grid-rows-[1fr_auto_3fr] sm:p-4">
+            <Headless.Transition.Child
+              enter="transition duration-100 ease-out"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="transition duration-100 ease-in"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Headless.DialogPanel
+                className={clsx(
+                  className,
+                  sizes[size],
+                  'row-start-2 w-full rounded-2xl bg-white p-8 shadow-lg ring-1 ring-zinc-950/10 sm:rounded-2xl sm:p-6 dark:bg-zinc-900 dark:ring-white/10 forced-colors:outline'
+                )}
+              >
+                {children}
+              </Headless.DialogPanel>
+            </Headless.Transition.Child>
+          </div>
         </div>
-      </div>
-    </Headless.Dialog>
+      </Headless.Dialog>
+    </Headless.Transition>
   )
 }
 
