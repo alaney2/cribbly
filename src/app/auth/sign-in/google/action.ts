936b9fafabf84752cd1445e6c4a426313bf191c6
Alan Yao <alanyao.training@gmail.com>
2024-07-17 23:21:03 -0400

    hardcode

--- src/app/auth/sign-in/google/action.ts
+++ src/app/auth/sign-in/google/action.ts
@@ -7,23 +7,23 @@ import { getURL } from '@/utils/helpers';
 export async function signInGoogle() {
   const supabase = createClient()
   const redirectURL = getURL('/auth/callback');
-  const newUrl = redirectURL + '?provider=google&next=/dashboard'
-  console.log(redirectURL)
-  redirect(newUrl)
-  // const { data, error } = await supabase.auth.signInWithOAuth({
-  //   provider: 'google', 
-  //   options: {
-  //     redirectTo: redirectURL,
-  //     queryParams: {
-  //       // access_type: 'offline',
-  //       // prompt: 'consent',
-  //     },
-  //   },
-  // })
+  // const newUrl = redirectURL + '?provider=google&next=/dashboard'
+  // console.log(redirectURL)
+  // redirect(newUrl)
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google', 
+    options: {
+      redirectTo: "https://www.cribbly.io/auth/callback",
+      queryParams: {
+        // access_type: 'offline',
+        // prompt: 'consent',
+      },
+    },
+  })
 
-  // if (data && data.url) {
-  //   redirect(data.url);
-  // } else {
-  //   console.error("Authentication failed or no data received", error);
-  // }
+  if (data && data.url) {
+    redirect(data.url);
+  } else {
+    console.error("Authentication failed or no data received", error);
+  }
 }
