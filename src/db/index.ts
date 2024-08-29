0cc4ec34eccd324666da9e918db646b491a7520e
Alan Yao <alanyao.training@gmail.com>
2024-08-28 21:38:14 -0400

    remove drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,9 @@
 // import { env } from "@/env.js"
-import { drizzle } from "drizzle-orm/postgres-js"
-import postgres from "postgres"
+// import { drizzle } from "drizzle-orm/postgres-js"
+// import postgres from "postgres"
 
-import * as schema from "./schema"
-const connectionString = process.env.DATABASE_URL || ''
+// import * as schema from "./schema"
+// const connectionString = process.env.DATABASE_URL || ''
 
-const client = postgres(connectionString, { prepare: false })
-export const db = drizzle(client, { schema })
+// const client = postgres(connectionString, { prepare: false })
+// export const db = drizzle(client, { schema })
