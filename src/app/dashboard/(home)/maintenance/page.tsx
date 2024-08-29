c81a3c482b551c21014b9d73589f8996b5e08167
Alan Yao <alanyao.training@gmail.com>
2024-08-28 21:09:12 -0400

    maintenance

--- src/app/dashboard/(home)/maintenance/page.tsx
+++ src/app/dashboard/(home)/maintenance/page.tsx
@@ -1,20 +1,10 @@
-// import { getTasks } from "@/app/_lib/queries"
-import { searchParamsSchema } from "@/app/_lib/validations"
 import type { SearchParams } from "@/types"
-import { TasksTable } from "@/app/_components/tasks-table"
-import { DataTableSkeleton } from "@/components/data-table/data-table-skeleton"
 import * as React from "react"
-import { DateRangePicker } from '@/components/data-table/date-range-picker'
-import { TasksTableProvider } from "@/app/_components/tasks-table-provider"
 import { MaintenanceTable } from "@/components/maintenance/MaintenanceTable"
 import { getTasks } from "@/utils/supabase/actions"
 
-export interface IndexPageProps {
-  params: { property_id: string }
-  searchParams: SearchParams
-}
 
-export default async function Maintenance({ params, searchParams } : IndexPageProps ) {
+export default async function Maintenance() {
   const data = await getTasks()
   return (
     <>
