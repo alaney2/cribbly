db0981af0c0b7287c0fe1e88899e6059ca7a34b1
Alan Yao <alanyao.training@gmail.com>
2024-08-28 22:51:02 -0400

    key

--- src/components/maintenance/MaintenanceTable.tsx
+++ src/components/maintenance/MaintenanceTable.tsx
@@ -1,19 +1,47 @@
-"use client"
+'use client'
 import React, { useState } from 'react'
 import { Button } from '@/components/catalyst/button'
-import { Dialog, DialogActions, DialogBody, DialogDescription, DialogTitle } from '@/components/catalyst/dialog'
+import {
+  Dialog,
+  DialogActions,
+  DialogBody,
+  DialogDescription,
+  DialogTitle,
+} from '@/components/catalyst/dialog'
 import { Input } from '@/components/catalyst/input'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/catalyst/table'
-import { Description, Field, FieldGroup, Fieldset, Label, Legend } from '@/components/catalyst/fieldset'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/catalyst/table'
+import {
+  Description,
+  Field,
+  FieldGroup,
+  Fieldset,
+  Label,
+  Legend,
+} from '@/components/catalyst/fieldset'
 import { Select } from '@/components/catalyst/select'
 import { Text } from '@/components/catalyst/text'
 import { Textarea } from '@/components/catalyst/textarea'
 import { Radio, RadioField, RadioGroup } from '@/components/catalyst/radio'
 import { Heading, Subheading } from '@/components/catalyst/heading'
-import { Checkbox, CheckboxField, CheckboxGroup } from '@/components/catalyst/checkbox'
-import { Listbox, ListboxLabel, ListboxOption } from '@/components/catalyst/listbox'
+import {
+  Checkbox,
+  CheckboxField,
+  CheckboxGroup,
+} from '@/components/catalyst/checkbox'
+import {
+  Listbox,
+  ListboxLabel,
+  ListboxOption,
+} from '@/components/catalyst/listbox'
 import { createTask } from '@/utils/supabase/actions'
-import { toast } from 'sonner';
+import { toast } from 'sonner'
 // const initialRequests = [
 //   { id: 1, date: '2024-08-18', title: 'Leaky Faucet', description: 'The kitchen faucet is leaking', status: 'Pending', priority: 'Medium' },
 //   { id: 2, date: '2024-08-17', title: 'Broken Window', description: 'Living room window is cracked', status: 'In Progress', priority: 'High' },
@@ -32,10 +60,10 @@ type Request = {
   notify: boolean
 }
 
-export function MaintenanceTable({ tasks, key } : { tasks: Request[]; key: string }) {
-  const [requests, setRequests] = useState<Request[]>(tasks);
-  const [isNewDialogOpen, setIsNewDialogOpen] = useState(false);
-  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+export function MaintenanceTable({ tasks }: { tasks: Request[] }) {
+  const [requests, setRequests] = useState<Request[]>(tasks)
+  const [isNewDialogOpen, setIsNewDialogOpen] = useState(false)
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false)
   const [currentRequest, setCurrentRequest] = useState({
     id: 0,
     updated_at: new Date(),
@@ -43,49 +71,82 @@ export function MaintenanceTable({ tasks, key } : { tasks: Request[]; key: strin
     description: '',
     status: '',
     priority: '',
-    notify: false
-  });
+    notify: false,
+  })
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
-    const { name, value } = e.target;
-    setCurrentRequest((prev) => ({ ...prev, [name]: value }));
-  };
+  const handleInputChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >,
+  ) => {
+    const { name, value } = e.target
+    setCurrentRequest((prev) => ({ ...prev, [name]: value }))
+  }
 
   const handleSubmit = () => {
     // e.preventDefault();
     if (currentRequest.id === 0) {
       // New request
-      const newId = requests.length + 1;
-      const currentDate = new Date();
+      const newId = requests.length + 1
+      const currentDate = new Date()
       setRequests([
         ...requests,
-        { ...currentRequest, id: newId, updated_at: currentDate, status: 'Pending' },
-      ]);
+        {
+          ...currentRequest,
+          id: newId,
+          updated_at: currentDate,
+          status: 'Pending',
+        },
+      ])
     } else {
       // Edit existing request
-      setRequests(requests.map(req => 
-        req.id === currentRequest.id ? currentRequest : req
-      ));
+      setRequests(
+        requests.map((req) =>
+          req.id === currentRequest.id ? currentRequest : req,
+        ),
+      )
     }
-    setIsNewDialogOpen(false);
-    setIsEditDialogOpen(false);
-    setCurrentRequest({ id: 0, updated_at: new Date(), title: '', description: '', status: '', priority: '', notify: false });
-  };
+    setIsNewDialogOpen(false)
+    setIsEditDialogOpen(false)
+    setCurrentRequest({
+      id: 0,
+      updated_at: new Date(),
+      title: '',
+      description: '',
+      status: '',
+      priority: '',
+      notify: false,
+    })
+  }
 
   const handleRowClick = (request: Request) => {
-    setCurrentRequest(request);
-    setIsEditDialogOpen(true);
-  };
+    setCurrentRequest(request)
+    setIsEditDialogOpen(true)
+  }
 
   return (
     <div className="p-4">
       <Heading className="mb-4">Maintenance Requests</Heading>
-      <Button onClick={() => {
-        setCurrentRequest({ id: 0, updated_at: new Date(), title: '', description: '', status: '', priority: 'Medium', notify: false });
-        setIsNewDialogOpen(true);
-      }} className="mb-4" color="blue">New Request</Button>
-      
-      <Table bleed grid >
+      <Button
+        onClick={() => {
+          setCurrentRequest({
+            id: 0,
+            updated_at: new Date(),
+            title: '',
+            description: '',
+            status: '',
+            priority: 'Medium',
+            notify: false,
+          })
+          setIsNewDialogOpen(true)
+        }}
+        className="mb-4"
+        color="blue"
+      >
+        New Request
+      </Button>
+
+      <Table bleed grid>
         <TableHead>
           <TableRow>
             <TableHeader>Date</TableHeader>
@@ -96,7 +157,11 @@ export function MaintenanceTable({ tasks, key } : { tasks: Request[]; key: strin
         </TableHead>
         <TableBody>
           {requests.map((request) => (
-            <TableRow key={request.id} onClick={() => handleRowClick(request)} className="cursor-default hover:bg-gray-50">
+            <TableRow
+              key={request.id}
+              onClick={() => handleRowClick(request)}
+              className="cursor-default hover:bg-gray-50"
+            >
               <TableCell>{String(request.created_at)}</TableCell>
               <TableCell>{request.title}</TableCell>
               <TableCell>{request.status}</TableCell>
@@ -106,29 +171,41 @@ export function MaintenanceTable({ tasks, key } : { tasks: Request[]; key: strin
         </TableBody>
       </Table>
 
-      <Dialog open={isNewDialogOpen || isEditDialogOpen} onClose={() => {
-        setIsNewDialogOpen(false);
-        setIsEditDialogOpen(false);
-      }}>
-        <DialogTitle>{currentRequest.id === 0 ? 'New Maintenance Request' : 'Edit Maintenance Request'}</DialogTitle>
+      <Dialog
+        open={isNewDialogOpen || isEditDialogOpen}
+        onClose={() => {
+          setIsNewDialogOpen(false)
+          setIsEditDialogOpen(false)
+        }}
+      >
+        <DialogTitle>
+          {currentRequest.id === 0
+            ? 'New Maintenance Request'
+            : 'Edit Maintenance Request'}
+        </DialogTitle>
         <DialogBody>
-          <form action={async (formData) => {
-            toast.promise(new Promise(async (resolve, reject) => {
-              try {
-                const data = await createTask(formData)
-                resolve('Success');
-              } catch (error) {
-                reject(error)
-              } finally {
-                handleSubmit()
-              }
-            }), {
-              loading: 'Adding...',
-              success: 'Task added!',
-              error: 'An error occurred, please check the form and try again.'
-            })
-            
-          }}>
+          <form
+            action={async (formData) => {
+              toast.promise(
+                new Promise(async (resolve, reject) => {
+                  try {
+                    const data = await createTask(formData)
+                    resolve('Success')
+                  } catch (error) {
+                    reject(error)
+                  } finally {
+                    handleSubmit()
+                  }
+                }),
+                {
+                  loading: 'Adding...',
+                  success: 'Task added!',
+                  error:
+                    'An error occurred, please check the form and try again.',
+                },
+              )
+            }}
+          >
             <FieldGroup>
               <Field>
                 <Label htmlFor="title">Title</Label>
@@ -150,7 +227,7 @@ export function MaintenanceTable({ tasks, key } : { tasks: Request[]; key: strin
                   required
                 />
               </Field>
-              
+
               <Field>
                 <Label htmlFor="priority">Priority</Label>
                 <Select
@@ -185,7 +262,7 @@ export function MaintenanceTable({ tasks, key } : { tasks: Request[]; key: strin
                   <Select
                     id="status"
                     name="status"
-                    defaultValue={"Pending"}
+                    defaultValue={'Pending'}
                     onChange={handleInputChange}
                   >
                     <option value="Pending">Pending</option>
@@ -203,15 +280,22 @@ export function MaintenanceTable({ tasks, key } : { tasks: Request[]; key: strin
               )}
             </FieldGroup>
             <DialogActions>
-              <Button type="button" onClick={() => {
-                setIsNewDialogOpen(false);
-                setIsEditDialogOpen(false);
-              }}>Cancel</Button>
-              <Button type="submit" color="blue">Submit</Button>
+              <Button
+                type="button"
+                onClick={() => {
+                  setIsNewDialogOpen(false)
+                  setIsEditDialogOpen(false)
+                }}
+              >
+                Cancel
+              </Button>
+              <Button type="submit" color="blue">
+                Submit
+              </Button>
             </DialogActions>
           </form>
         </DialogBody>
       </Dialog>
     </div>
-  );
+  )
 }
