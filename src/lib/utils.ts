2e60a838623225876241802c74d174fbcb36fcde
Alan Yao <alanyao.training@gmail.com>
2024-08-28 22:01:36 -0400

    remove db

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,36 +12,3 @@ export function generateId({ length = 8, prefix = "" } = {}) {
     length
   )()}`
 }
-
-export function formatDate(
-  date: Date | string | number,
-  options: Intl.DateTimeFormatOptions = {}
-) {
-  return new Intl.DateTimeFormat("en-US", {
-    month: options.month ?? "long",
-    day: options.day ?? "numeric",
-    year: options.year ?? "numeric",
-    ...options,
-  }).format(new Date(date))
-}
-
-/**
- * Stole this from the @radix-ui/primitive
- * @see https://github.com/radix-ui/primitives/blob/main/packages/core/primitive/src/primitive.tsx
- */
-export function composeEventHandlers<E>(
-  originalEventHandler?: (event: E) => void,
-  ourEventHandler?: (event: E) => void,
-  { checkForDefaultPrevented = true } = {}
-) {
-  return function handleEvent(event: E) {
-    originalEventHandler?.(event)
-
-    if (
-      checkForDefaultPrevented === false ||
-      !(event as unknown as Event).defaultPrevented
-    ) {
-      return ourEventHandler?.(event)
-    }
-  }
-}
