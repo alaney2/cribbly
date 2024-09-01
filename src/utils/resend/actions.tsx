eb75556b01e549f5408351a23a01a19f8be4c90f
Alan Yao <alanyao.training@gmail.com>
2024-08-31 22:39:46 -0400

    yaml

--- src/utils/resend/actions.tsx
+++ src/utils/resend/actions.tsx
@@ -1,20 +1,21 @@
-"use server"
-import { Resend } from 'resend';
-import InviteUserEmail from '@/components/PropertySettings/InviteUserEmail';
-import { createClient } from '@/utils/supabase/server';
-import { getUser } from '@/utils/supabase/actions';
-import { generateId } from "@/lib/utils"
-import { unstable_noStore as noStore } from "next/cache";
+'use server'
+import { Resend } from 'resend'
+import InviteUserEmail from '@/components/PropertySettings/InviteUserEmail'
+import { createClient } from '@/utils/supabase/server'
+import { getUser } from '@/utils/supabase/actions'
+import { generateId } from '@/lib/utils'
+import { unstable_noStore as noStore } from 'next/cache'
 
 export async function sendInviteEmail(formData: FormData) {
-  noStore();
+  noStore()
   const supabase = createClient()
   const user = await getUser()
   if (!user) {
     throw new Error('User not found')
   }
 
-  const { data: user_data } = await supabase.from('users')
+  const { data: user_data } = await supabase
+    .from('users')
     .select()
     .eq('id', user.id)
     .single()
@@ -23,10 +24,11 @@ export async function sendInviteEmail(formData: FormData) {
   }
 
   const email = String(formData.get('email'))
-  const fullName = String(formData.get('fullName'));
+  const fullName = String(formData.get('fullName'))
   const propertyId = String(formData.get('propertyId'))
   if (!propertyId) throw new Error('Property not found')
-  const { data: property } = await supabase.from('properties')
+  const { data: property } = await supabase
+    .from('properties')
     .select()
     .eq('id', propertyId)
     .single()
@@ -41,30 +43,35 @@ export async function sendInviteEmail(formData: FormData) {
     from: 'Cribbly.io <support@cribbly.io>',
     reply_to: 'support@cribbly.io',
     to: email,
-    subject: property?.street_address + (property?.apt ? ` ${property.apt}` : '') + ' Invitation',
-    react: <InviteUserEmail 
-      invitedByUsername={user_data.full_name || ''}
-      invitedByEmail={user_data.email}
-      username={fullName}
-      teamName={property?.street_address + (property?.apt ? ` ${property.apt}` : '')}
-      inviteLink={`https://cribbly.io/invite?property=${propertyId}&token=${token}&email=${email}&name=${fullName}&address=${property.street_address} ${property.apt}`}
-    />,
-  });
+    subject:
+      property?.street_address +
+      (property?.apt ? ` ${property.apt}` : '') +
+      ' Invitation',
+    react: (
+      <InviteUserEmail
+        invitedByUsername={user_data.full_name || ''}
+        invitedByEmail={user_data.email}
+        username={fullName}
+        teamName={
+          property?.street_address + (property?.apt ? ` ${property.apt}` : '')
+        }
+        // inviteLink={`https://resident.cribbly.io/invite?property=${propertyId}&token=${token}&email=${email}&name=${fullName}&address=${property.street_address} ${property.apt}`}
+        inviteLink={`http://localhost:3001/invite?property=${propertyId}&token=${token}&email=${email}&name=${fullName}&address=${property.street_address} ${property.apt}`}
+      />
+    ),
+  })
 
-  
   if (error) {
-    console.error(error);
+    console.error(error)
     throw new Error('Error sending email')
   }
 
-  const { error: tokenError } = await supabase
-    .from('property_invites')
-    .upsert({
-      token,
-      full_name: fullName,
-      email,
-      property_id: propertyId,
-    })
+  const { error: tokenError } = await supabase.from('property_invites').upsert({
+    token,
+    full_name: fullName,
+    email,
+    property_id: propertyId,
+  })
 
   if (tokenError) {
     throw new Error('Error creating invite')
