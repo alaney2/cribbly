35d658c285aadd06f0e3862a8490cf7306ee46a3
Alan Yao <alanyao.training@gmail.com>
2024-09-02 18:31:59 -0400

    mware

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,71 +1,44 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
-import { type NextRequest, NextResponse } from 'next/server';
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { type NextRequest, NextResponse } from "next/server";
 
 export const createClient = (request: NextRequest) => {
-  // Create an unmodified response
-  let response = NextResponse.next({
-    request: {
-      headers: request.headers
-    }
-  });
+	// Create an unmodified response
+	let supabaseResponse = NextResponse.next({
+		request,
+	});
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          // If the cookie is updated, update the cookies for the request and response
-          request.cookies.set({
-            name,
-            value,
-            ...options
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers
-            }
-          });
-          response.cookies.set({
-            name,
-            value,
-            ...options
-          });
-        },
-        remove(name: string, options: CookieOptions) {
-          // If the cookie is removed, update the cookies for the request and response
-          request.cookies.set({
-            name,
-            value: '',
-            ...options
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers
-            }
-          });
-          response.cookies.set({
-            name,
-            value: '',
-            ...options
-          });
-        }
-      }
-    }
-  );
+	const supabase = createServerClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
+		{
+			cookies: {
+				getAll() {
+					return request.cookies.getAll();
+				},
+				setAll(cookiesToSet) {
+					for (const { name, value, options } of cookiesToSet) {
+						request.cookies.set(name, value);
+					}
+					supabaseResponse = NextResponse.next({
+						request,
+					});
+					for (const { name, value, options } of cookiesToSet) {
+						supabaseResponse.cookies.set(name, value, options);
+					}
+				},
+			},
+		},
+	);
 
-  return { supabase, response };
+	return { supabase, response: supabaseResponse };
 };
 
 export const updateSession = async (request: NextRequest) => {
-  const { supabase, response } = createClient(request);
+	const { supabase, response } = createClient(request);
 
-  // This will refresh session if expired - required for Server Components
-  // https://supabase.com/docs/guides/auth/server-side/nextjs
-  await supabase.auth.getUser();
+	// This will refresh session if expired - required for Server Components
+	// https://supabase.com/docs/guides/auth/server-side/nextjs
+	await supabase.auth.getUser();
 
-  return response;
+	return response;
 };
