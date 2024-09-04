2aac188ab53d4b60343ac81a5bdd804766dde471
Alan Yao <alanyao.training@gmail.com>
2024-09-03 22:10:06 -0400

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,95 +1,95 @@
-import '@/styles/globals.css'
-import { Inter, Lexend, Poppins } from 'next/font/google'
-import localFont from 'next/font/local'
-import clsx from 'clsx'
-import { Analytics } from '@vercel/analytics/react';
+import "@/styles/globals.css";
+import { Inter, Lexend, Poppins } from "next/font/google";
+import localFont from "next/font/local";
+import clsx from "clsx";
+import { Analytics } from "@vercel/analytics/react";
 // import '@/styles/mapbox-gl.css';
-import '@/styles/tailwind.css'
-import { type Metadata } from 'next'
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import Script from 'next/script';
-import { PropsWithChildren, Suspense } from 'react';
-import { Toaster } from "@/components/ui/sonner"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
-
+import "@/styles/tailwind.css";
+import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
+import type { PropsWithChildren } from "react";
+import { Toaster } from "@/components/ui/sonner";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
 
 export const metadata: Metadata = {
-  title: {
-    template: '%s - Cribbly',
-    default: 'Property Management Software for Small Landlords - Cribbly',
-  },
-  description:
-    'Cribbly is the all-in-one property management software designed for small landlords. Its powerful platform automates rent collection, maintenance requests, and more.',
-  metadataBase: new URL('https://cribbly.io'),
-  openGraph: {
-    title: 'Cribbly | Property Management Software for Small Landlords',
-    description: 'Property management software for small landlords',
-    url: 'https://cribbly.io',
-    siteName: 'Cribbly',
-    images: [
-      {
-        url: 'https://cribbly.io/images/opengraph-image.png',
-      }
-    ],
-    locale: 'en_US',
-    type: 'website'
-  },
-}
+	title: {
+		template: "%s - Cribbly",
+		default: "Property Management Software for Small Landlords - Cribbly",
+	},
+	description:
+		"Cribbly is the all-in-one property management software designed for small landlords. Its powerful platform automates rent collection, maintenance requests, and more.",
+	metadataBase: new URL("https://cribbly.io"),
+	openGraph: {
+		title: "Cribbly | Property Management Software for Small Landlords",
+		description: "Property management software for small landlords",
+		url: "https://cribbly.io",
+		siteName: "Cribbly",
+		images: [
+			{
+				url: "https://cribbly.io/images/opengraph-image.png",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
+};
 
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
-})
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-inter",
+});
 
 const lexend = Lexend({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-lexend',
-})
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-lexend",
+});
 
 const openRunde = localFont({
-  src: [
-    {
-      path: '../../public/fonts/open-runde/OpenRunde-Regular.otf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/open-runde/OpenRunde-Medium.otf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/open-runde/OpenRunde-Semibold.otf',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/open-runde/OpenRunde-Bold.otf',
-      weight: '700',
-      style: 'normal',
-    },  ],
-  variable: '--font-open-runde',
-})
+	src: [
+		{
+			path: "../../public/fonts/open-runde/OpenRunde-Regular.otf",
+			weight: "400",
+			style: "normal",
+		},
+		{
+			path: "../../public/fonts/open-runde/OpenRunde-Medium.otf",
+			weight: "500",
+			style: "normal",
+		},
+		{
+			path: "../../public/fonts/open-runde/OpenRunde-Semibold.otf",
+			weight: "600",
+			style: "normal",
+		},
+		{
+			path: "../../public/fonts/open-runde/OpenRunde-Bold.otf",
+			weight: "700",
+			style: "normal",
+		},
+	],
+	variable: "--font-open-runde",
+});
 
 export default function RootLayout({ children }: PropsWithChildren) {
-  return (
-    <html
-      lang="en"
-      className={clsx(
-        'h-full scroll-smooth bg-gray-50',
-        inter.variable,
-        lexend.variable,
-        openRunde.variable,
-      )}
-    >
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-MTXEMYC4EM"
-      />
-      <Script id="gtag">
-        {`
+	return (
+		<html
+			lang="en"
+			className={clsx(
+				"h-full scroll-smooth bg-gray-50",
+				inter.variable,
+				lexend.variable,
+				openRunde.variable,
+			)}
+		>
+			<Script
+				async
+				src="https://www.googletagmanager.com/gtag/js?id=G-MTXEMYC4EM"
+			/>
+			<Script id="gtag">
+				{`
           window.dataLayer = window.dataLayer || [];
           function gtag() {
             dataLayer.push(arguments);
@@ -97,16 +97,16 @@ export default function RootLayout({ children }: PropsWithChildren) {
           gtag('js', new Date());
           gtag('config', 'G-MTXEMYC4EM');
         `}
-      </Script>
+			</Script>
 
-      <body className="h-full bg-gray-50 subpixel-antialiased">
-        {children}
+			<body className="h-full bg-gray-50 subpixel-antialiased">
+				{children}
 
-        <Analytics />
-        <SpeedInsights />
-        <Toaster position="top-center" />
-        <TailwindIndicator />
-      </body>
-    </html>
-  )
+				<Analytics />
+				<SpeedInsights />
+				<Toaster position="top-center" />
+				<TailwindIndicator />
+			</body>
+		</html>
+	);
 }
