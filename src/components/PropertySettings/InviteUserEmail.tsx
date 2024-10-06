3252e7850343af290bc7305da4b278e9ebce498d
Alan Yao <alanyao.training@gmail.com>
2024-10-05 23:38:37 -0400

    logo

--- src/components/PropertySettings/InviteUserEmail.tsx
+++ src/components/PropertySettings/InviteUserEmail.tsx
@@ -1,112 +1,112 @@
 import {
-  Body,
-  Button,
-  Container,
-  Column,
-  Head,
-  Heading,
-  Hr,
-  Html,
-  Img,
-  Link,
-  Preview,
-  Row,
-  Section,
-  Text,
+	Body,
+	Button,
+	Container,
+	Column,
+	Head,
+	Heading,
+	Hr,
+	Html,
+	Img,
+	Link,
+	Preview,
+	Row,
+	Section,
+	Text,
 } from "@react-email/components";
 import { Tailwind } from "@react-email/tailwind";
 import * as React from "react";
 
 interface InviteUserEmailProps {
-  username?: string;
-  userImage?: string;
-  invitedByUsername?: string;
-  invitedByEmail?: string;
-  teamName?: string;
-  teamImage?: string;
-  inviteLink?: string;
-  inviteFromIp?: string;
-  inviteFromLocation?: string;
+	username?: string;
+	userImage?: string;
+	invitedByUsername?: string;
+	invitedByEmail?: string;
+	teamName?: string;
+	teamImage?: string;
+	inviteLink?: string;
+	inviteFromIp?: string;
+	inviteFromLocation?: string;
 }
 
 const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "";
+	? `https://${process.env.VERCEL_URL}`
+	: "";
 
 export const InviteUserEmail = ({
-  username,
-  userImage,
-  invitedByUsername,
-  invitedByEmail,
-  teamName,
-  teamImage,
-  inviteLink,
-  inviteFromIp,
-  inviteFromLocation,
+	username,
+	userImage,
+	invitedByUsername,
+	invitedByEmail,
+	teamName,
+	teamImage,
+	inviteLink,
+	inviteFromIp,
+	inviteFromLocation,
 }: InviteUserEmailProps) => {
-  const previewText = `Join ${invitedByUsername} on Cribbly`;
+	const previewText = `Join ${invitedByUsername} on Cribbly`;
 
-  return (
-    <Html>
-      <Head />
-      <Preview>{previewText}</Preview>
-      <Tailwind>
-        <Body className="bg-white my-auto mx-auto font-sans px-2">
-          <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[465px]">
-            <Section className="mt-[32px]">
-              <Img
-                src={'https://www.cribbly.io/images/logo-text.png'}
-                width="200"
-                height="110"
-                alt="Cribbly"
-                className="my-0 mx-auto"
-              />
-            </Section>
-            <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-              Join <strong>{teamName}</strong> on <strong>Cribbly</strong>
-            </Heading>
-            <Text className="text-black text-[14px] leading-[24px]">
-              Hello {username}!
-            </Text>
-            <Text className="text-black text-[14px] leading-[24px]">
-              <strong>{invitedByUsername}</strong> (
-              <Link
-                href={`mailto:${invitedByEmail}`}
-                className="text-blue-600 no-underline"
-              >
-                {invitedByEmail}
-              </Link>
-              ) has invited you to join <strong>{teamName}</strong> on{" "}
-              Cribbly. 
-            </Text>
-            <Text className="text-black text-[14px] leading-[24px]">
-              Cribbly is a property management platform that helps you pay rent and fees, track expenses, and communicate with your landlord.
-            </Text>
-            <Section className="text-center mt-[32px] mb-[32px]">
-              <Button
-                className="bg-blue-500 rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3"
-                href={inviteLink}
-              >
-                Join property
-              </Button>
-            </Section>
-            {/* <Text className="text-black text-[14px] leading-[24px]">
+	return (
+		<Html>
+			<Head />
+			<Preview>{previewText}</Preview>
+			<Tailwind>
+				<Body className="bg-white my-auto mx-auto font-sans px-2">
+					<Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[465px]">
+						<Section className="mt-[32px]">
+							<Img
+								src={"https://www.cribbly.io/images/logo_text.png"}
+								width="200"
+								height="110"
+								alt="Cribbly"
+								className="my-0 mx-auto"
+							/>
+						</Section>
+						<Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
+							Join <strong>{teamName}</strong> on <strong>Cribbly</strong>
+						</Heading>
+						<Text className="text-black text-[14px] leading-[24px]">
+							Hello {username}!
+						</Text>
+						<Text className="text-black text-[14px] leading-[24px]">
+							<strong>{invitedByUsername}</strong> (
+							<Link
+								href={`mailto:${invitedByEmail}`}
+								className="text-blue-600 no-underline"
+							>
+								{invitedByEmail}
+							</Link>
+							) has invited you to join <strong>{teamName}</strong> on Cribbly.
+						</Text>
+						<Text className="text-black text-[14px] leading-[24px]">
+							Cribbly is a property management platform that helps you pay rent
+							and fees, track expenses, and communicate with your landlord.
+						</Text>
+						<Section className="text-center mt-[32px] mb-[32px]">
+							<Button
+								className="bg-blue-500 rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3"
+								href={inviteLink}
+							>
+								Join property
+							</Button>
+						</Section>
+						{/* <Text className="text-black text-[14px] leading-[24px]">
               or copy and paste this URL into your browser:{" "}
               <Link href={inviteLink} className="text-blue-600 no-underline">
                 {inviteLink}
               </Link>
             </Text> */}
-            <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
-            <Text className="text-[#666666] text-[12px] leading-[24px]">
-              This invitation was intended for{" "}
-              <span className="text-black">{username}</span>. If you
-              were not expecting this invitation, you can safely ignore this email.
-            </Text>
-          </Container>
-        </Body>
-      </Tailwind>
-    </Html>
-  );
+						<Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
+						<Text className="text-[#666666] text-[12px] leading-[24px]">
+							This invitation was intended for{" "}
+							<span className="text-black">{username}</span>. If you were not
+							expecting this invitation, you can safely ignore this email.
+						</Text>
+					</Container>
+				</Body>
+			</Tailwind>
+		</Html>
+	);
 };
 
 // InviteUserEmail.PreviewProps = {
