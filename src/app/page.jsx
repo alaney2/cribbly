7388dddc9794bf23f17c61328b617dc299e37f1c
Alan Yao <alan.yao@servicenow.com>
2024-10-17 21:31:15 -0400

    bg

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,62 +13,64 @@ import { Container } from "@/components/default/Container";
 import { FollowerPointerCard } from "@/components/aceternity/following-pointer";
 import { WavyBackground } from "@/components/aceternity/wavy-background";
 import { BeforeAfter } from "@/components/landing/BeforeAfter";
+import { BentoFeatures } from "@/components/landing/BentoFeatures";
 import Image from "next/image";
 import { Button } from "@/components/catalyst/button";
 import backgroundImage from "@/images/background-call-to-action.jpg";
 import { AnimatedBackground } from "@/components/landing/AnimatedBackground";
 
 export default function Home() {
-	return (
-		<div className="bg-gray-50">
-			<AnimatedBackground>
-				<Header />
-				<Hero />
-			</AnimatedBackground>
-			<main>
-				<PrimaryFeatures />
-				<BeforeAfter />
-				<Testimonials />
-				<FollowerPointerCard title="🏠 Future Property Manager">
-					{/* <CallToAction /> */}
-					<section
-						id="get-started-today"
-						className="relative overflow-hidden bg-blue-600 py-32"
-					>
-						<Image
-							className="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
-							src={backgroundImage}
-							alt=""
-							width={2347}
-							height={1244}
-							unoptimized
-						/>
-						<Container className="relative">
-							<div className="mx-auto max-w-lg text-center">
-								<h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
-									May the Rent be with you
-								</h2>
-								<p className="mt-4 text-lg tracking-tight text-white">
-									Harness the power of effortless property management. Let
-									Cribbly be the force that drives your success.
-								</p>
-								<Button
-									href="/get-started"
-									color="sky"
-									className={"mt-10 cursor-none"}
-								>
-									Yup, I'm in
-								</Button>
-							</div>
-						</Container>
-					</section>
-				</FollowerPointerCard>
+  return (
+    <div className="bg-gray-50">
+      <AnimatedBackground>
+        <Header />
+        <Hero />
+      </AnimatedBackground>
+      <main>
+        <PrimaryFeatures />
+        <BeforeAfter />
+        {/* <BentoFeatures /> */}
+        <Testimonials />
+        <FollowerPointerCard title="🏠 Future Property Manager">
+          {/* <CallToAction /> */}
+          <section
+            id="get-started-today"
+            className="relative overflow-hidden bg-blue-600 py-32"
+          >
+            <Image
+              className="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
+              src={backgroundImage}
+              alt=""
+              width={2347}
+              height={1244}
+              unoptimized
+            />
+            <Container className="relative">
+              <div className="mx-auto max-w-lg text-center">
+                <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
+                  May the Rent be with you
+                </h2>
+                <p className="mt-4 text-lg tracking-tight text-white">
+                  Harness the power of effortless property management. Let
+                  Cribbly be the force that drives your success.
+                </p>
+                <Button
+                  href="/get-started"
+                  color="sky"
+                  className={"mt-10 cursor-none"}
+                >
+                  Yup, I'm in
+                </Button>
+              </div>
+            </Container>
+          </section>
+        </FollowerPointerCard>
 
-				{/* <SecondaryFeatures /> */}
-				{/* <Pricing /> */}
-				<Faqs />
-			</main>
-			<Footer />
-		</div>
-	);
+        {/* <SecondaryFeatures /> */}
+        {/* <Pricing /> */}
+        <Faqs />
+      </main>
+      <Footer />
+    </div>
+  );
 }
--- src/components/landing/BeforeAfter.tsx
+++ src/components/landing/BeforeAfter.tsx
@@ -1,123 +1,123 @@
 import Image from "next/image";
 import { Container } from "@/components/default/Container";
 import {
-	XMarkIcon,
-	CheckIcon,
-	ArrowRightIcon,
+  XMarkIcon,
+  CheckIcon,
+  ArrowRightIcon,
 } from "@heroicons/react/24/outline";
 import backgroundImage from "@/images/background-faqs.jpg";
 
 const beforeBenefits = [
-	"Spending time and energy keeping track of rent payments",
-	"Having unorganized documents and financials",
-	"Getting calls and texts about maintenance requests at inconvenient times",
-	"Complicated and time-consuming tax preparation",
+  "Spending time and energy keeping track of rent payments",
+  "Having unorganized documents and financials",
+  "Getting calls and texts about maintenance requests at inconvenient times",
+  "Complicated and time-consuming tax preparation",
 ];
 
 const afterBenefits = [
-	"Automatic rent collection through direct deposits",
-	"One central source of truth for all documents and financials",
-	"Centralized maintenance request system accessible from any device",
-	"Financial reports and automatic tax calculations for an easy tax season",
+  "Automatic rent collection through direct deposits",
+  "One central source of truth for all documents and financials",
+  "Centralized maintenance request system accessible from any device",
+  "Financial reports and automatic tax calculations for an easy tax season",
 ];
 
 export function BeforeAfter() {
-	return (
-		<section
-			id="benefits"
-			aria-label="We make property management easy."
-			className="relative bg-slate-100 text-slate-900 pt-20 pb-8 sm:pt-32 overflow-hidden shadow-sm"
-		>
-			<Container>
-				<div className="mx-auto md:max-w-4xl md:text-center z-20">
-					<h2 className="font-display text-3xl sm:text-4xl font-medium tracking-tighter text-slate-800 bg-gradient-to-r to-blue-600 from-[#60a4ff] via-indigo-400 inline-block text-transparent bg-clip-text p-4">
-						Indubitibbly the right choice
-					</h2>
-				</div>
-				<div className="mx-auto z-10 mt-8 lg:mt-12 flex justify-center max-w-4xl w-full">
-					<div className="w-full p-8 rounded-xl outline outline-gray-300 outline-1 z-10 bg-slate-50">
-						<div className="md:hidden">
-							<div className="text-lg sm:text-xl mb-1 text-blue-300">From</div>
-							<div className="text-2xl sm:text-3xl font-semibold tracking-tight text-gray-800 mb-2">
-								Without Cribbly
-							</div>
-							<div className="divide-y">
-								{beforeBenefits.map((beforeBenefit, index) => (
-									<div
-										// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-										key={index}
-										className="text-md sm:text-lg py-4 flex items-start"
-									>
-										<span className="h-4 w-4 mr-2 mt-1">
-											<XMarkIcon className="h-4 w-4 text-red-600" />
-										</span>
-										<span>{beforeBenefit}</span>
-									</div>
-								))}
-							</div>
-							<div className="text-lg sm:text-xl mb-1 mt-8 text-blue-300">
-								To
-							</div>
-							<div className="text-2xl sm:text-3xl font-semibold tracking-tight text-gray-800 mb-2">
-								With Cribbly
-							</div>
-							<div className="divide-y">
-								{afterBenefits.map((afterBenefit, index) => (
-									<div
-										// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-										key={index}
-										className="text-md sm:text-lg py-4 flex items-start"
-									>
-										<span className="h-4 w-4 mr-2 mt-1">
-											<CheckIcon className="h-4 w-4 text-green-500" />
-										</span>
-										<span>{afterBenefit}</span>
-									</div>
-								))}
-							</div>
-						</div>
-						<div className="hidden md:block">
-							<div className="flex">
-								<div className="w-5/12 border-b-0">
-									<div className="text-xl mb-1 text-blue-300">From</div>
-									<div className="text-3xl text-gray-800 font-semibold tracking-tight">
-										Without Cribbly
-									</div>
-								</div>
-								<div className="w-1/6 border-b-0" />
-								<div className="w-5/12 border-b-0">
-									<div className="text-xl mb-1 text-blue-300">To</div>
-									<div className="text-3xl text-gray-800 font-semibold tracking-tight">
-										With Cribbly
-									</div>
-								</div>
-							</div>
-							<div className="divide-y mt-4">
-								{beforeBenefits.map((beforeBenefit, index) => (
-									// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-									<div key={index} className="text-lg pb-6 pt-4 flex">
-										<div className="w-5/12 border-b-0 gap-x-2 flex items-start">
-											<span className="h-5 w-5 mt-1 mr-1">
-												<XMarkIcon className="h-5 w-5 text-red-600" />
-											</span>
-											<span>{beforeBenefit}</span>
-										</div>
-										<div className="w-1/6 border-b-0 text-center flex justify-center">
-											<ArrowRightIcon className="h-8 w-8 text-gray-300 mx-4" />
-										</div>
-										<div className="w-5/12 border-b-0 flex items-start gap-x-2">
-											<span className="h-5 w-5 mt-1 mr-1">
-												<CheckIcon className="h-5 w-5 text-green-500" />
-											</span>
-											<span>{afterBenefits[index]}</span>
-										</div>
-									</div>
-								))}
-							</div>
-						</div>
-					</div>
-				</div>
-			</Container>
-		</section>
-	);
+  return (
+    <section
+      id="benefits"
+      aria-label="We make property management easy."
+      className="relative py-20 sm:py-32 overflow-hidden shadow-sm bg-gray-100 text-zinc-800 "
+    >
+      <Container>
+        <div className="mx-auto md:max-w-4xl md:text-center z-20">
+          <h2 className="font-display text-3xl sm:text-4xl font-medium tracking-tighter text-slate-800 bg-gradient-to-r to-blue-600 from-[#60a4ff] via-indigo-400 inline-block text-transparent bg-clip-text p-4">
+            Indubitibbly the right choice
+          </h2>
+        </div>
+        <div className="mx-auto z-10 mt-8 lg:mt-12 flex justify-center max-w-4xl w-full">
+          <div className="w-full p-8 rounded-xl outline outline-gray-300 outline-1 z-10 bg-slate-50">
+            <div className="md:hidden">
+              <div className="text-lg sm:text-xl mb-1 text-blue-300">From</div>
+              <div className="text-2xl sm:text-3xl font-semibold tracking-tight text-gray-800 mb-2">
+                Without Cribbly
+              </div>
+              <div className="divide-y">
+                {beforeBenefits.map((beforeBenefit, index) => (
+                  <div
+                    // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                    key={index}
+                    className="text-md sm:text-lg py-4 flex items-start"
+                  >
+                    <span className="h-4 w-4 mr-2 mt-1">
+                      <XMarkIcon className="h-4 w-4 text-red-600" />
+                    </span>
+                    <span>{beforeBenefit}</span>
+                  </div>
+                ))}
+              </div>
+              <div className="text-lg sm:text-xl mb-1 mt-8 text-blue-300">
+                To
+              </div>
+              <div className="text-2xl sm:text-3xl font-semibold tracking-tight text-gray-800 mb-2">
+                With Cribbly
+              </div>
+              <div className="divide-y">
+                {afterBenefits.map((afterBenefit, index) => (
+                  <div
+                    // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                    key={index}
+                    className="text-md sm:text-lg py-4 flex items-start"
+                  >
+                    <span className="h-4 w-4 mr-2 mt-1">
+                      <CheckIcon className="h-4 w-4 text-green-500" />
+                    </span>
+                    <span>{afterBenefit}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="hidden md:block">
+              <div className="flex">
+                <div className="w-5/12 border-b-0">
+                  <div className="text-xl mb-1 text-blue-300">From</div>
+                  <div className="text-3xl text-gray-800 font-semibold tracking-tight">
+                    Without Cribbly
+                  </div>
+                </div>
+                <div className="w-1/6 border-b-0" />
+                <div className="w-5/12 border-b-0">
+                  <div className="text-xl mb-1 text-blue-300">To</div>
+                  <div className="text-3xl text-gray-800 font-semibold tracking-tight">
+                    With Cribbly
+                  </div>
+                </div>
+              </div>
+              <div className="divide-y mt-4">
+                {beforeBenefits.map((beforeBenefit, index) => (
+                  // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                  <div key={index} className="text-lg pb-6 pt-4 flex">
+                    <div className="w-5/12 border-b-0 gap-x-2 flex items-start">
+                      <span className="h-5 w-5 mt-1 mr-1">
+                        <XMarkIcon className="h-5 w-5 text-red-600" />
+                      </span>
+                      <span>{beforeBenefit}</span>
+                    </div>
+                    <div className="w-1/6 border-b-0 text-center flex justify-center">
+                      <ArrowRightIcon className="h-8 w-8 text-gray-300 mx-4" />
+                    </div>
+                    <div className="w-5/12 border-b-0 flex items-start gap-x-2">
+                      <span className="h-5 w-5 mt-1 mr-1">
+                        <CheckIcon className="h-5 w-5 text-green-500" />
+                      </span>
+                      <span>{afterBenefits[index]}</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
 }
