94a0820bc36ac3c88ab868289338e61aa1141469
Alan Yao <alanyao.training@gmail.com>
2024-10-20 16:47:23 -0400

    clean up

--- src/app/dashboard/(home)/settings/general/page.tsx
+++ src/app/dashboard/(home)/settings/general/page.tsx
@@ -1,10 +1,7 @@
 "use server";
 import SettingsNavigation from "@/components/SettingsNavigation";
-import { getCurrentProperty, getPlaidAccounts } from "@/utils/supabase/actions";
 
 export default async function TenantSettings() {
-	// const currentPropertyId = await getCurrentProperty();
-
 	return (
 		<main className="flex max-w-7xl flex-col justify-center space-y-4 sm:space-y-8">
 			<SettingsNavigation initialTab="General" />
