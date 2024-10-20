dec61ed34b2ebf46c315bde6e3e763396e3b0f63
Alan Yao <alanyao.training@gmail.com>
2024-10-20 16:50:23 -0400

    clean up

--- src/app/dashboard/(home)/settings/tenants/page.tsx
+++ src/app/dashboard/(home)/settings/tenants/page.tsx
@@ -1,27 +1,10 @@
 "use server";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 import SettingsNavigation from "@/components/SettingsNavigation";
-import { getCurrentProperty, getPlaidAccounts } from "@/utils/supabase/actions";
 
 export default async function TenantSettings() {
-	// const supabase = createClient();
-	// const currentPropertyId = await getCurrentProperty();
-
-	// const { data: lease, error: leaseError } = await supabase
-	// 	.from("leases")
-	// 	.select("*")
-	// 	.eq("property_id", currentPropertyId)
-	// 	.single();
-
-	// const plaidAccounts = await getPlaidAccounts();
-
 	return (
 		<main className="flex max-w-7xl flex-col justify-center space-y-4 sm:space-y-8">
-			<SettingsNavigation
-				// key={currentPropertyId}
-				initialTab="Tenants"
-			/>
+			<SettingsNavigation initialTab="Tenants" />
 		</main>
 	);
 }
